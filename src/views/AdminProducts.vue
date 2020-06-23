57c46be8bc8280815e4bea400594c28199fc2561
Paulo <8630187+papawlo@users.noreply.github.com>
2020-06-23 15:56:57 +0100

    feat: 37 - realtime update  in firestore

--- src/views/AdminProducts.vue
+++ src/views/AdminProducts.vue
@@ -39,16 +39,79 @@
               <th scope="col">ID</th>
               <th scope="col">Name</th>
               <th scope="col">Price</th>
+              <th scope="col">Modify</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(product,index) in products" :key="index">
               <th scope="row">{{index}}</th>
-              <td>{{product.name}}</td>
-              <td>{{product.price}}</td>
+              <td>{{product.data().name}}</td>
+              <td>{{product.data().price}}</td>
+              <td>
+                <button
+                  @click="editProduct(product)"
+                  class="btn btn-primary"
+                  data-toggle="modal"
+                  data-target="#editModal"
+                >Edit</button>
+
+                <button @click="deleteProduct(product.id)" class="btn btn-danger ml-3">Delete</button>
+              </td>
             </tr>
           </tbody>
         </table>
+      </div>
+    </div>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#editModal"
+    >Launch demo modal</button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="editModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="editModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <form>
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editModalLabel">Edit Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <input
+                  type="text"
+                  placeholder="Product Name"
+                  v-model="product.name"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group">
+                <input type="text" placeholder="Price" v-model="product.price" class="form-control" />
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                @click="closeModal"
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >Close</button>
+              <button @click="updateProduct" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -56,6 +119,7 @@
 
 <script>
 import firebase from "firebase";
+import jQuery from "jquery";
 var db = firebase.firestore();
 
 export default {
@@ -66,16 +130,63 @@ export default {
       product: {
         name: null,
         price: null
-      }
+      },
+      activeItem: null
     };
   },
   methods: {
+    closeModal() {
+      this.resetData();
+    },
+    watcher() {
+      db.collection("products").onSnapshot(querySnapshot => {
+        this.products = [];
+        querySnapshot.forEach(doc => {
+          this.products.push(doc);
+        });
+      });
+    },
+    updateProduct() {
+      db.collection("products")
+        .doc(this.activeItem)
+        .update(this.product)
+        .then(() => {
+          console.log("Successfully updated");
+          jQuery("#editModal").modal("hide");
+          this.readData();
+        })
+        .catch(err => {
+          console.log(("deu merda: ", err));
+        });
+    },
+    editProduct(product) {
+      // jQuery("#editMoral").modal("show");
+      this.product = product.data();
+      this.activeItem = product.id;
+      console.log(product);
+    },
+    deleteProduct(id) {
+      console.log(id);
+      if (confirm("Are yoou sure?")) {
+        db.collection("products")
+          .doc(id)
+          .delete()
+          .then(() => {
+            console.log("Document deleted Successfuly");
+            // this.readData();
+          })
+          .catch(err => {
+            console.error(" Error removieng document: ", err);
+          });
+      }
+    },
     readData() {
       db.collection("products")
         .get()
         .then(querySnapshot => {
+          this.products = [];
           querySnapshot.forEach(doc => {
-            this.products.push(doc.data());
+            this.products.push(doc);
             // console.log(`${doc.id} => ${doc.data()}`);
           });
         });
@@ -91,13 +202,15 @@ export default {
         .catch(error => {
           console.error("Error adding document: ", error);
         });
+    },
+    resetData() {
+      this.product.name = this.product.price = this.activeItem = null;
+      //   // Object.assign(this.$data, this.$options.data.apply(this));
     }
-    // resetData() {
-    //   // Object.assign(this.$data, this.$options.data.apply(this));
-    // }
   },
   created() {
-    this.readData();
+    // this.readData();
+    this.watcher();
   }
 };
 </script>
