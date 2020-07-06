dd9151771476a9c3f1981a8dc47a2ed6689e8a0a
Paulo <8630187+papawlo@users.noreply.github.com>
2020-07-06 18:56:07 +0100

    feat: 77- Finally, Stripe Checkou

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -25,6 +25,7 @@
                 </div>
               </li>
             </ul>
+            <button class="pay-with-stripe btn btn-primary mt-4" @click="pay">Pay with credit card</button>
           </div>
           <div class="col-md-3">
             <p>content</p>
@@ -41,14 +42,55 @@
 // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
 import Login from "@/components/LoginModal.vue";
+import axios from "axios";
+import Stripe from "stripe";
+const stripe = Stripe("pk_test_3yyWicMGKdLdRD28kHM3ANkX00vOa4k2B3");
 export default {
   name: "home",
   components: {
     NavBar,
     Login
+  },
+  data() {
+    return {
+      sessionId: ""
+    };
+  },
+  methods: {
+    pay() {
+      // data = {id:10,id:10}
+      let data = this.$store.state.cart.map(item => ({
+        [item.productId]: item.productQuantity
+      }));
+      data = Object.assign({}, ...data);
+      axios
+        .get(
+          "https://us-central1-vue-shop-5a95e.cloudfunctions.net/CheckoutSession",
+          {
+            params: {
+              products: data
+            }
+          }
+        )
+        .then(response => {
+          this.sessionId = response.data;
+          console.log(response.data);
+          stripe
+            .redirectToCheckout({
+              sessionId: this.sessionId.id
+            })
+            .then(function(result) {
+              console.log(result);
+            });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
+
 
 <style scoped>
 </style>
